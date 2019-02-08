d6705ce6661f63b3ea61864fe7b7a5f2333f4e48
Klas Marcks <klas.marcks@gmail.com>
2019-02-08 19:59:31 +0100

    Created build script for windows and disabeled unvanted compnents

---
+++ RadMapBuildWin.bat
@@ -0,0 +1,3 @@
+rem RUN from a clean prompt where you have initialized vcvars64-bat
+rem edit components do add/remove build components
+CALL buildwin 150 build static_mt release x64 nosamples notests msbuild
